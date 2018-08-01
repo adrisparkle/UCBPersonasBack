e8d2c3552832c9c58f9f75ea93efd13e62fd4dd0
Flakoash <flash.ar79@gmail.com>
2018-08-01 17:14:15 -0400

    modified batch

--- UcbBack/copyfiles.cmd
+++ UcbBack/copyfiles.cmd
@@ -4,28 +4,28 @@ set rootpath=%~dp0
 set destination="C:\Users\Adrian\Desktop\www"
 
 mkdir "%destination%\Areas"
-robocopy "%rootpath%\Areas" "%destination%\Areas" /E
+robocopy "%rootpath%\Areas" "%destination%\Areas" /E /COPYALL
 
 mkdir "%destination%\bin"
-robocopy "%rootpath%\bin" "%destination%\bin" /E
+robocopy "%rootpath%\bin" "%destination%\bin" /E /COPYALL
 
 mkdir "%destination%\Content"
-robocopy "%rootpath%\Content" "%destination%\Content" /E
+robocopy "%rootpath%\Content" "%destination%\Content" /E /COPYALL
 
 mkdir "%destination%\fonts"
-robocopy "%rootpath%\fonts" "%destination%\fonts" /E
+robocopy "%rootpath%\fonts" "%destination%\fonts" /E /COPYALL
 
 mkdir "%destination%\Images"
-robocopy "%rootpath%\Images" "%destination%\Images" /E
+robocopy "%rootpath%\Images" "%destination%\Images" /E /COPYALL
 
 mkdir "%destination%\Scripts"
-robocopy "%rootpath%\Scripts" "%destination%\Scripts" /E
+robocopy "%rootpath%\Scripts" "%destination%\Scripts" /E /COPYALL
 
 mkdir "%destination%\Static"
-robocopy "%rootpath%\Static" "%destination%\Static" /E
+robocopy "%rootpath%\Static" "%destination%\Static" /E /COPYALL
 
 mkdir "%destination%\Views"
-robocopy "%rootpath%\Views" "%destination%\Views" /E
+robocopy "%rootpath%\Views" "%destination%\Views" /E /COPYALL
 
 robocopy "C:\Program Files (x86)\Jenkins\workspace\Windows Service Deployment\UcbBack" "C:\Users\Adrian\Desktop\www" favicon.ico
 robocopy "%rootpath%\" %destination% Global.asax
